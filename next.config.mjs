02b99ff02f81d457631942ef88526ace318077ba
Albert <albert@192.168.0.64>
2026-05-27 20:50:01 +0800

    Configure quote base path

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: '/quote',
+  assetPrefix: '/quote',
   distDir: process.env.NEXT_DIST_DIR ?? '.next',
 };
 
